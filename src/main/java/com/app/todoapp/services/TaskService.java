0f3ed4149b531ac5990108baf6ecbd27e1f385c5
sanketh360 <sankethkottary3519@gmail.com>
2026-03-27 14:08:27 +0530

    my local changes

--- src/main/java/com/app/todoapp/services/TaskService.java
+++ src/main/java/com/app/todoapp/services/TaskService.java
@@ -32,7 +32,7 @@ public class TaskService {
 
     public void toggleTask(Long id) {
         Task task = taskRepository.findById(id)
-                .orElseThrow(()->new IllegalArgumentException("Invadil task id"));
+                .orElseThrow(()->new IllegalArgumentException("wrong tash Id"));
         task.setCompleted(!task.isCompleted());
         taskRepository.save(task);
     }
